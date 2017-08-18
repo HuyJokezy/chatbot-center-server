d480a7b8412441d075fd3f6ef347d9b44264345d
HuyJokezy <nqhuy1811@gmail.com>
2017-08-18 18:36:12 +0700

    Test Stage

--- fbmsg/handler.js
+++ fbmsg/handler.js
@@ -45,6 +45,7 @@ exports.processInputMessage = function (messaging) {
 }
 
 function sendMessage (message) {
+	console.log(message);
 	let options = {
 		method: 'POST',
 		uri: "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=" + PAGE_ACCESS_TOKEN,
