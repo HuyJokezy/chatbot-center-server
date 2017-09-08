732826fce876c24415a55e9e0045b2ad45cc8f8b
HuyJokezy <nqhuy1811@gmail.com>
2017-09-08 16:14:33 +0700

    Webview

--- fbmsg/configure.js
+++ fbmsg/configure.js
@@ -100,6 +100,7 @@ exports.persistentMenu = function (PAGE_ACCESS_TOKEN, showProductButtonTitle, we
 			]
 		}
 	};
+	console.log(options.body);
 	request(options, function (error, response, body) {
 		if (error) {
 			console.log('Error: Cannot set Persistent Menu');
